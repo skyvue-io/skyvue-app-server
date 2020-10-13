58038be3042772cf5c1b223f197f06eab6f39cd8
Tristan Tarpley <tristan@learnwithjuni.com>
2020-10-12 19:15:06 -0500

    added revoke route for user authentication

--- src/routes/auth/userService.ts
+++ src/routes/auth/userService.ts
@@ -11,6 +11,7 @@ const saltRounds = 10;
 import stripeService from "../../services/stripeService";
 import * as EmailValidator from 'email-validator';
 import createTokens from "./lib/createTokens";
+import verifyRefreshToken from './lib/verifyRefreshToken';
 
 /**
  * /auth/user routes
@@ -29,7 +30,7 @@ router.post('/create', async (req, res) => {
     await reqSchema.validateAsync(req.body);
   } catch (err) {
     res.status(400).json({
-      message: 'Missing parameters',
+      error: 'Missing parameters',
     });
   }
 
@@ -38,7 +39,7 @@ router.post('/create', async (req, res) => {
   const emailValid = EmailValidator.validate(email);
   if (!emailValid) {
     return res.status(400).json({
-      message: "email_invalid",
+      error: "email_invalid",
     })
   }
 
@@ -59,10 +60,10 @@ router.post('/create', async (req, res) => {
     console.error(e);
 
     if (e.code === 11000) {
-      return res.status(400).json({ message: "email_exists" });
+      return res.status(400).json({ error: "email_exists" });
     }
     
-    return res.status(400).json({ message: "unknown_error" });
+    return res.status(400).json({ error: "unknown_error" });
   }
 
   const stripeCustomer = await stripeService.createCustomer({
@@ -98,7 +99,7 @@ router.post('/login', async (req, res) => {
     await reqSchema.validateAsync(req.body);
   } catch (err) {
     res.status(400).json({
-      message: 'Missing parameters',
+      error: 'Missing parameters',
     });
   }
 
@@ -109,18 +110,18 @@ router.post('/login', async (req, res) => {
   }).exec();
 
   if (!user) {
-    return res.status(400).json({ message: "bad_login" });
+    return res.status(400).json({ error: "bad_login" });
   }
 
   const passwordValid = await bcrypt.compare(password, user.password);
 
   if (!passwordValid) {
-    return res.status(400).json({ message: "bad_login" });
+    return res.status(400).json({ error: "bad_login" });
   }
 
   const userWithUpdatedCount = await User.findByIdAndUpdate(user._id, {
     refreshAuthCount: user.refreshAuthCount + 1,
-  }).exec();
+  }, { new: true }).exec();
 
   const { refreshToken, accessToken } = createTokens({
     userId: user._id,
@@ -129,6 +130,69 @@ router.post('/login', async (req, res) => {
   });
 
   return res.status(200).json({ refreshToken, accessToken });
+})
+
+router.post('/refresh', async (req, res) => {
+  const reqSchema = Joi.object({
+    refreshToken: Joi.string().required(),
+  })
+
+  try {
+    await reqSchema.validateAsync(req.body);
+  } catch (err) {
+    res.status(400).json({
+      error: 'Missing parameters',
+    });
+  }
+
+  const { refreshToken } = req.body;
+  const decodedRefreshToken = verifyRefreshToken(refreshToken, process.env.JWT_SECRET);
+
+  if (decodedRefreshToken.error) {
+    return res.json({
+      error: decodedRefreshToken.name,
+    })
+  }
+
+  const user = await User.findById(decodedRefreshToken.userId).lean().exec();
+  if (user.refreshAuthCount !== decodedRefreshToken.count) {
+    return res.status(401).json({ error: 'Authorization error' })
+  }
+
+  const userWithUpdatedCount = await User.findByIdAndUpdate(user._id, {
+    refreshAuthCount: user.refreshAuthCount + 1,
+  }, { new: true }).exec();
+  
+  const newTokens = createTokens({
+    userId: user._id,
+    email: decodedRefreshToken.email,
+    count: userWithUpdatedCount.refreshAuthCount,
+  });
+
+  res.json({ ...newTokens });
+})
+
+router.post('/revokeToken', async (req, res) => {
+  const reqSchema = Joi.object({
+    userId: Joi.string().required(),
+  })
+
+  try {
+    await reqSchema.validateAsync(req.body);
+  } catch (err) {
+    res.status(400).json({
+      error: 'Missing parameters',
+    });
+  }
+
+  const { userId } = req.body;
+  const user = await User.findById(userId).lean().exec();
+
+  await User.findByIdAndUpdate(userId, {
+    refreshAuthCount: user.refreshAuthCount + 1,
+  }).exec();
+
+  res.sendStatus(200);
 })
 
 module.exports = router;
