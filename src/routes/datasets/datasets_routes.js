eac243e62ce0d016d4f797b6009e6fd0f5ac040d
Tristan Tarpley <tristan@learnwithjuni.com>
2020-11-04 10:57:25 -0600

    csv uploads are being parsed properly!

--- src/routes/datasets/datasets_routes.js
+++ src/routes/datasets/datasets_routes.js
@@ -1,26 +1,25 @@
 const express = require('express');
-const csv = require('csv-parser');
+const csv = require('csvtojson');
 const fs = require('fs');
+const expressUpload = require('express-fileupload');
 
 const results = [];
 
 const router = new express.Router();
 
+router.use(expressUpload());
 router.post('/upload', (req, res) => {
-  const { file } = req.body;
-  const buff = Buffer.from(file, 'base64');
-  const text = buff.toString('ascii');
-  console.log(text, file);
-  // fs.createReadStream('data.csv')
-  //   .pipe(csv())
-  //   .on('data', data => results.push(data))
-  //   .on('end', () => {
-  //     console.log(results);
-  //     // [
-  //     //   { NAME: 'Daffy Duck', AGE: '24' },
-  //     //   { NAME: 'Bugs Bunny', AGE: '22' }
-  //     // ]
-  //   });
+  csv()
+    .fromString(req.files.csv.data.toString('utf8'))
+    .on('data', data => {
+      const jsonStr = data.toString('utf8');
+      console.log(jsonStr);
+    })
+    .on('done', () => {
+      console.log('done parsing');
+    });
+
+  res.sendStatus(200);
 });
 
 module.exports = router;
