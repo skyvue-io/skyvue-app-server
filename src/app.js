794773f956bd7ff4dfcb18f52665d8c9882338c8
Tristan Tarpley <tristan@learnwithjuni.com>
2021-02-06 14:47:27 -0600

    change cors options

--- src/app.js
+++ src/app.js
@@ -11,16 +11,23 @@ require('./models').connect(config.dbUri);
 
 const PORT = process.env.PORT || 5000;
 
-const corsOptions = {
-  origin:
-    process.env.ENVIRONMENT === 'development'
-      ? 'http://localhost:3000'
-      : 'https://app.skyvue.io',
-  optionsSuccessStatus: 200,
+const CORS_LOOKUP = {
+  development: ['http://localhost:3000'],
+  staging: [],
+  production: 'app.skyvue.io',
 };
 
-// app.set('trust proxy', true);
-app.use(cors(corsOptions));
+const corsOptionsDelegate = function (req, callback) {
+  const allowList = CORS_LOOKUP[process.env.ENVIRONMENT];
+  const corsOptions =
+    req.header('Origin').includes('netlify.app') ||
+    allowList.indexOf(req.header('Origin')) !== -1
+      ? { origin: true }
+      : { origin: false };
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
+
+app.use(cors(corsOptionsDelegate));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
