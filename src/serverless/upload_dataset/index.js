475e45808d9c2204d104663e364ec4562b1564c4
Tristan Tarpley <tristan@learnwithjuni.com>
2021-03-14 20:00:18 -0500

    testing

--- src/serverless/upload_dataset/index.js
+++ src/serverless/upload_dataset/index.js
@@ -2,7 +2,7 @@ exports.handler = async event => {
   console.log('hello world');
   const response = {
     statusCode: 200,
-    body: JSON.stringify('This is a test'),
+    body: JSON.stringify('This is a test!!!'),
   };
   return response;
 };
