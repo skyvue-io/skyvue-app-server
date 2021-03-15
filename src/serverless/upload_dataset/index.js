288a6c48fd7b73deebbcd89987be48934b52ccbc
Tristan Tarpley <tristan@learnwithjuni.com>
2021-03-14 19:56:53 -0500

    test

--- src/serverless/upload_dataset/index.js
+++ src/serverless/upload_dataset/index.js
@@ -1,8 +1,8 @@
 exports.handler = async event => {
-  // TODO implement
+  console.log('hello world');
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!!!'),
+    body: JSON.stringify('This is a test'),
   };
   return response;
 };
