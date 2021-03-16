fd86d7f3919060dac54b9238c7727efa6021d808
Tristan Tarpley <tristan@learnwithjuni.com>
2021-03-15 21:04:25 -0500

    remove serverless

--- src/serverless/upload_dataset/index.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async event => {
-  console.log('hello world');
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify('This is a test!!!'),
-  };
-  return response;
-};
